689004e82823b9c6ad0a580fec03d32b52af6b5e
Michael <mpeder75@gmail.com>
2023-09-28 19:18:39 +0200

    Section 4 created

---
+++ Session 2/n. Out keyword/Program.cs
@@ -0,0 +1,42 @@
+﻿
+/* Out keyword bruges ex. Hvis man ønsker at returnere 2 resultater fra em method */
+
+
+// Eksempel: Array med positive og negative tal,
+/* 
+ man øsker at returnere positive tal i et array,
+ og en integer der returnere hvor mange negative tal der er 
+*/
+
+var numbers = new[] { 10, -8, 2, 12, -17 };         // Array med tal man vil tjekke
+
+int nonPositiveCount;                               // Opbevare non positive integers
+var onlyPositive = GetOnlyPositiveNumbers(numbers, out nonPositiveCount);
+for (int i = 0; i < onlyPositive.Count; i++)
+{
+    var positiveNumber = onlyPositive[i];
+    Console.WriteLine(positiveNumber);
+}
+
+Console.WriteLine($"Count of non-positive numbers: {nonPositiveCount}");
+
+
+// Her bruges out i method decklaration: out int countOfNonPositive
+List<int> GetOnlyPositiveNumbers(int[] numbers, out int countOfNonPositive)
+{
+    countOfNonPositive = 0;
+    var result = new List<int>();
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            result.Add(numbers[i]);
+        } else
+        {
+            countOfNonPositive++;
+        }
+    }
+
+    return result;
+}
---
+++ Session 2/o. TryParse method/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// TryParse med out parameter
+/*
+Console.WriteLine("Enter a number");
+var userInput = Console.ReadLine();
+
+bool isParsingSuccesful = int.TryParse(userInput, out int number);
+
+if (isParsingSuccesful)
+{
+    Console.WriteLine("Paresing worked, numeber is: " + number);
+}
+else
+{
+    Console.WriteLine("Parsing NOT succesful");
+}
+
+*/
+
+// Eksempel: Få bruger til at inputte valid input
+
+bool isParsingSuccesful;
+
+do
+{
+    Console.WriteLine("Enter a number");
+    var userInput = Console.ReadLine();
+
+    isParsingSuccesful = int.TryParse(userInput, out int number);
+    if (isParsingSuccesful)
+    {
+        Console.WriteLine("Parsing worked, number is: " + number);
+    }
+    else
+    {
+        Console.WriteLine("Not parsed correct");
+    }
+} while (!isParsingSuccesful);
+
+var name;
